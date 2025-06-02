64fce374b8c015022c9d0fca1fd799e83346ff92
Stephan Maximilian Huber <stephan@factorial.io>
2025-06-02 12:05:11 +0200

    Update scottyctl.rb

--- scottyctl.rb
+++ scottyctl.rb
@@ -1,26 +1,26 @@
 class Scottyctl < Formula
   desc "CLI tool for scotty PaaS"
   homepage "https://github.com/factorial-io/scotty"
-  version "0.1.0-alpha.30"
+  version "0.1.0-alpha.32"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/factorial-io/scotty/releases/download/v#{version}/scottyctl-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/factorial-io/scotty/releases/download/v0.1.0-alpha.32/scottyctl-aarch64-apple-darwin.tar.gz"
       # Replace with actual SHA256 after release
-      sha256 "REPLACE_AARCH64_APPLE_SHA"
+      sha256 "ed265624abac42c8c348bdc1f812b3fd74e7f210c382e673d9d0984288118a7c"
     elsif Hardware::CPU.intel?
-      url "https://github.com/factorial-io/scotty/releases/download/v#{version}/scottyctl-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/factorial-io/scotty/releases/download/v0.1.0-alpha.32/scottyctl-x86_64-apple-darwin.tar.gz"
       # Replace with actual SHA256 after release
-      sha256 "REPLACE_X86_64_APPLE_SHA"
+      sha256 "c3a1b0548537d2295eb44895619e3a92bf8d2df8e50e9e3353cbdf57351f90e7"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/factorial-io/scotty/releases/download/v#{version}/scottyctl-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/factorial-io/scotty/releases/download/v0.1.0-alpha.32/scottyctl-x86_64-unknown-linux-gnu.tar.gz"
       # Replace with actual SHA256 after release
-      sha256 "REPLACE_X86_64_LINUX_GNU_SHA"
+      sha256 "788890375f2f68698fa5b8d8c1f224f5f54ca5ff7cf03b9290eb597066721d7e"
     end
   end
 
